993603471f1a62b9fd5c7d8c57403f266a9d043f
Massimiliano Giovagnoli <me@maxgio.it>
2022-06-20 19:20:35 +0200

    cleanup(internal/scrape.go): remove useless commented code
    Signed-off-by: Massimiliano Giovagnoli <me@maxgio.it>

--- internal/utils/scrape.go
+++ internal/utils/scrape.go
@@ -7,7 +7,6 @@ import (
 	v "github.com/spf13/viper"
 )
 
-//func GetScrapeConfigFromViper(distro string, viper *v.Viper) (scrape.Config, error) {
 func GetScrapeConfigFromViper(distroName string, viper *v.Viper) (distro.Config, error) {
 	if distroName == "" {
 		return distro.Config{}, fmt.Errorf("configuration is not valid")
