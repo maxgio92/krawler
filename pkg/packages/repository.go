8ea772e4f47bac3957fcc1c11cc529c972c1fa37
Massimiliano Giovagnoli <me@maxgio.it>
2022-09-03 12:26:46 +0200

    refactor(pkg/packages): move repository under packages package
    Signed-off-by: Massimiliano Giovagnoli <me@maxgio.it>

---
+++ pkg/packages/repository.go
@@ -0,0 +1,15 @@
+package packages
+
+type Repository struct {
+	Name string
+	URI  URITemplate
+}
+
+type URITemplate string
+
+type Mirror struct {
+	Name string
+	// The base URL of the package mirror
+	// (e.g. https://mirrors.kernel.org/<distribution>)
+	URL string
+}
