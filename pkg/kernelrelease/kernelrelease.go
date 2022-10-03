3ab301f1d8dbcee692fe192da26047b166a7f498
Massimiliano Giovagnoli <me@maxgio.it>
2022-10-03 19:31:34 +0200

    style(pkg/kernelrelease): reformat code
    Signed-off-by: Massimiliano Giovagnoli <me@maxgio.it>

--- pkg/kernelrelease/kernelrelease.go
+++ pkg/kernelrelease/kernelrelease.go
@@ -26,9 +26,7 @@ type KernelRelease struct {
 	CompilerVersion  string `json:"compiler_version"`
 }
 
-var (
-	kernelVersionPattern = regexp.MustCompile(`(?P<fullversion>^(?P<version>0|[1-9]\d*)\.(?P<patchlevel>0|[1-9]\d*)\.(?P<sublevel>0|[1-9]\d*))(?P<fullextraversion>-(?P<extraversion>0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-_]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$`)
-)
+var kernelVersionPattern = regexp.MustCompile(`(?P<fullversion>^(?P<version>0|[1-9]\d*)\.(?P<patchlevel>0|[1-9]\d*)\.(?P<sublevel>0|[1-9]\d*))(?P<fullextraversion>-(?P<extraversion>0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-_]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$`)
 
 func (k *KernelRelease) BuildFromPackage(pkg p.Package) error {
 	k.PackageName = pkg.GetName()
