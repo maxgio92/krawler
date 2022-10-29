c438fe576972c6472dfa42b247173779028c1b43
Massimiliano Giovagnoli <me@maxgio.it>
2022-10-29 16:27:54 +0200

    chore(amazonlinux): add configuration parsing
    Signed-off-by: Massimiliano Giovagnoli <me@maxgio.it>

--- pkg/distro/constants.go
+++ pkg/distro/constants.go
@@ -5,6 +5,9 @@ const (
 	X86_64Arch Arch = "x86_64"
 
 	// Default architecture for which scrape for packages.
-	DefaultArch = X86_64Arch
-	CentosType  = "centos"
+	DefaultArch          = X86_64Arch
+	CentosType           = "centos"
+	AmazonLinuxV1Type    = "amazonlinux1"
+	AmazonLinuxV2Type    = "amazonlinux2"
+	AmazonLinuxV2022Type = "amazonlinux2022"
 )
