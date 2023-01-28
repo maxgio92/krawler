12938c06c9ad9d3fec5179a914d5c0b7f40f0b24
Massimiliano Giovagnoli <me@maxgio.it>
2023-01-28 13:26:03 +0100

    fix(pkg/distro): add missing ubuntu config building
    Signed-off-by: Massimiliano Giovagnoli <me@maxgio.it>

--- pkg/distro/constants.go
+++ pkg/distro/constants.go
@@ -4,13 +4,14 @@ import "github.com/maxgio92/krawler/pkg/packages"
 
 //nolint:nosnakecase
 const (
-	X86_64Arch packages.Architecture = "x86_64"
+	X8664Arch packages.Architecture = "x86_64"
 
 	// Default architecture for which scrape for packages.
-	DefaultArch          = X86_64Arch
+	DefaultArch          = X8664Arch
 	CentosType           = "centos"
 	AmazonLinuxV1Type    = "amazonlinux"
 	AmazonLinuxV2Type    = "amazonlinux2"
 	AmazonLinuxV2022Type = "amazonlinux2022"
 	DebianType           = "debian"
+	UbuntuType           = "ubuntu"
 )
