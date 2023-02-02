b110aac091c549d3b920248bdf57e9911716e6d2
Massimiliano Giovagnoli <me@maxgio.it>
2023-02-02 16:41:20 +0100

    fix(pkg/kernelrelease): get unique list besides different source package url
    Signed-off-by: Massimiliano Giovagnoli <me@maxgio.it>

---
+++ pkg/kernelrelease/version.go
@@ -0,0 +1,19 @@
+package kernelrelease
+
+import (
+	"fmt"
+	p "github.com/maxgio92/krawler/pkg/packages"
+)
+
+func versionStringFromPackage(pkg p.Package) string {
+	version := pkg.GetVersion()
+	if pkg.GetRelease() != "" {
+		version += fmt.Sprintf("-%s", pkg.GetRelease())
+	}
+
+	if pkg.GetArch() != "" {
+		version += fmt.Sprintf(".%s", pkg.GetArch())
+	}
+
+	return version
+}
