0c0125a226a7d4a72a64b3827135ab3828c29234
Massimiliano Giovagnoli <me@maxgio.it>
2023-06-04 00:31:27 +0200

    refactor(pkg/packages/alpm): implement async pkg search
    Signed-off-by: Massimiliano Giovagnoli <me@maxgio.it>

---
+++ pkg/packages/alpm/search.go
@@ -0,0 +1,30 @@
+package alpm
+
+import (
+	"github.com/maxgio92/krawler/pkg/packages"
+)
+
+type SearchOptions struct {
+	*packages.SearchOptions
+	packageNames []string
+}
+
+// NewSearchOptions returns a pointer to a SearchOptions object from a pointer to a packages.SearchOptions, and
+// overriding architectures and seedURLs.
+func NewSearchOptions(options *packages.SearchOptions, seedURLs []string, packageNames []string) *SearchOptions {
+	return &SearchOptions{
+		packages.NewSearchOptions(
+			options.PackageName(),
+			nil,
+			seedURLs,
+			options.Verbosity(),
+			options.ProgressMessage(),
+			options.PackageFileNames()...,
+		),
+		packageNames,
+	}
+}
+
+func (o *SearchOptions) PackageNames() []string {
+	return o.packageNames
+}
